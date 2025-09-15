32f6768dd4efdd1f2ae082f2b77c0ac23164ed26
giedrius <giedrius@inkodus.lt>
2025-09-15 23:10:41 +0300

    points and travels tables

--- sql/places.sql
+++
@@ -1,4 +0,0 @@
-create table places (
-    place_id int not null primary key auto_increment,
-    place_name varchar (128)
-);
---
+++ sql/points.sql
@@ -0,0 +1,4 @@
+create table points (
+    point_id int not null primary key auto_increment,
+    point_name varchar (128)
+);
