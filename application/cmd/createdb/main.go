687ad0fa29119e1b04000e1d707a48c6e84906ad
giedrius <giedrius@inkodus.lt>
2025-12-17 16:26:25 +0200

    Script for test database.

--- application/cmd/createdb/main.go
+++ application/cmd/createdb/main.go
@@ -5,6 +5,7 @@ import (
 	"darbelis.eu/persedimai/internal/database"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -38,6 +39,25 @@ func main() {
 	}
 
 	fmt.Printf("Database '%s' created successfully!\n", dbName)
+
+	// Get DBConfig to retrieve user credentials for GRANT PRIVILEGES
+	fmt.Println("Retrieving database configuration for granting privileges...")
+	dbConfig, err := di.NewDbConfig(dbName)
+	if err != nil {
+		fmt.Printf("Warning: Failed to retrieve database configuration: %v\n", err)
+		fmt.Println("Database created, but privileges were not granted.")
+		fmt.Println("You may need to grant privileges manually.")
+		os.Exit(0)
+	}
+
+	fmt.Printf("Granting privileges to user '%s'...\n", dbConfig.Username)
+	err = grantPrivileges(db, dbName, dbConfig)
+	if err != nil {
+		fmt.Printf("Error granting privileges: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Privileges granted successfully to user '%s' on database '%s'!\n", dbConfig.Username, dbName)
 }
 
 func createDatabase(db *database.Database, dbName string) error {
@@ -53,6 +73,34 @@ func createDatabase(db *database.Database, dbName string) error {
 	_, err = conn.Exec(query)
 	if err != nil {
 		return fmt.Errorf("failed to execute CREATE DATABASE: %w", err)
+	}
+
+	return nil
+}
+
+func grantPrivileges(db *database.Database, dbName string, dbConfig *database.DBConfig) error {
+	conn, err := db.GetConnection()
+	if err != nil {
+		return fmt.Errorf("failed to get database connection: %w", err)
+	}
+
+	// Sanitize parameters to prevent SQL injection
+	escapedDbName := database.MysqlRealEscapeString(dbName)
+	escapedUsername := database.MysqlRealEscapeString(dbConfig.Username)
+
+	// GRANT ALL PRIVILEGES ON database.* TO 'user'@'host'
+	query := fmt.Sprintf("GRANT ALL ON `%s`.* TO '%s'", escapedDbName, escapedUsername)
+	log.Println("The grant query is " + query)
+
+	_, err = conn.Exec(query)
+	if err != nil {
+		return fmt.Errorf("failed to execute GRANT PRIVILEGES: %w", err)
+	}
+
+	// Execute FLUSH PRIVILEGES to ensure changes take effect
+	_, err = conn.Exec("FLUSH PRIVILEGES")
+	if err != nil {
+		return fmt.Errorf("failed to execute FLUSH PRIVILEGES: %w", err)
 	}
 
 	return nil
---
+++ application/create_test_databases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+../bin/createdb  -env test_root -name test_small
+../bin/createdb  -env test_root -name test_normal
+../bin/createdb  -env test_root -name test_complete
+
+
